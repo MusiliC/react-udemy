24f86ac5cef5b2c230026bbda890b71f3686b43f
brian.musili <brian.musili@systechafrica.com>
2024-08-06 06:47:20 +0300

    Use reducer project

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1 className='text-2xl'>The React QUiz</h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/MyMain.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const MyMain = ({children}) => {
+  return (
+    <main className='Main'>
+
+    </main>
+  )
+}
+
+export default MyMain
