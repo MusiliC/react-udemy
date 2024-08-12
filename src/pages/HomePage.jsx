7c4fe3b667495ad239c92b65b91cab29b771ee11
brian.musili <brian.musili@systechafrica.com>
2024-08-12 08:39:16 +0300

    Routing

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>World wise</div>
+  )
+}
+
+export default HomePage
---
+++ src/pages/Pricing.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Pricing = () => {
+  return (
+    <div>Pricing</div>
+  )
+}
+
+export default Pricing
