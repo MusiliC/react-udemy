5602a1774eba6b5170c0033e5256227cf273ae0d
brian.musili <brian.musili@systechafrica.com>
2024-08-26 09:08:21 +0300

    Add cities list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -8,15 +8,43 @@ import Homepage from "./pages/Homepage";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import styles from "./components/PageNav.module.css";
+import CityList from "./components/CityList";
+
+const baseUrl = "http://localhost:9000";
 
 const App = () => {
+  const [cities, setCities] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchCities = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`${baseUrl}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Homepage />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>Hello from cities</p>} />
-          <Route path="cities" element={<p>Hello from cities</p>} />
+          <Route
+            index
+            element={<CityList cities={cities} loading={loading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} loading={loading} />}
+          />
           <Route path="countries" element={<p>Hello from countries list</p>} />
           <Route path="form" element={<p>Form Element</p>} />
         </Route>
