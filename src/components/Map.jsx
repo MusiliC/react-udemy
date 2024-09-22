ddc013310481b67f58eecbea2d4b9f1e16b20926
brian.musili <brian.musili@systechafrica.com>
2024-09-22 09:55:27 +0300

    Map leaflet in worldwise peoject

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,26 +1,42 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import styles from "./Map.module.css"
-import { useNavigate, useSearchParams } from 'react-router-dom'
+import React, { useState } from "react";
+import styles from "./Map.module.css";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useCities } from "../context/CitiesContext";
 
 const Map = () => {
-
   const navigate = useNavigate();
+  const { cities } = useCities();
 
   const [searchParams, setSearchParams] = useSearchParams();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
 
-  const lat = searchParams.get("lat")
-  const long = searchParams.get("lng")
+  const lat = searchParams.get("lat");
+  const long = searchParams.get("lng");
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")} >
-      <h1>Position: {lat}, {long}</h1>
-
-      <button onClick={() => {
-        setSearchParams({lat:23, lng: 50})
-      }}>Change Pos</button>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={false}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city, i) => (
+          <Marker position={[city.position.lat, city.position.lng]} key={i}>
+            <Popup>
+            <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
-  )
-}
+  );
+};
 
-export default Map
+export default Map;
